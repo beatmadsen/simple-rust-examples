aca19d1375f0ead50689174c90aa417467a21584
Erik Madsen <beatmadsen@gmail.com>
2016-07-12 00:00:56 +0200

    EM: work on parsing input

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,14 @@
-pub mod topic; 
+pub mod topic;
+use std::num::ParseIntError;
+
+
+fn parse_input(s: &str) -> Result<&str, &str> {
+    let mut iter = s.split(", ");
+
+    match (iter.next(), iter.next()) {
+        (Some(topic), Some(n)) => {
+            Ok("") // TODO: parse n
+        },
+        _ => Err("Bad input")
+    }
+}
