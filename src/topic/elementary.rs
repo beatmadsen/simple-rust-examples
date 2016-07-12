8ff9a90d383b948e5d2bd2ac4b8aa1dffbce75c6
Erik Madsen <beatmadsen@gmail.com>
2016-07-13 00:00:30 +0200

    EM: skeleton of example 2

--- src/topic/elementary.rs
+++ src/topic/elementary.rs
@@ -6,8 +6,20 @@ impl Topic for Elementary {
 
     fn run_example(&self, n: u8) {
 
-        println!("Input was: {}", n);
+        println!("Topic: Elementary, example {}", n);
+        match n {
+            1 => { println!("Hello, world!"); },
+            2 => { example_2(); },
+            _ => { println!("Example not known or not implemented!"); }
+        };
     }
 
     fn describe(&self) -> String { "elementary".to_string() }
+}
+
+
+
+fn example_2() {
+    println!("Todo");
+        // TODO
 }
